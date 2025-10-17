38973e4b5fd0100dfd82cc75d87a0aaa0016dd50
seraj <serajstudy1@gmail.com>
2025-10-17 11:10:33 -0400

    updated countrycard

--- src/Components/CountryCard.jsx
+++ src/Components/CountryCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function CountryCard() {
   return (
     <footer className="bg-dark text-white text-center py-3 mt-5">
-      <p>© 2025 BaseEducation Consultancy. All rights reserved.</p>
+      <p>how are you</p>
     </footer>
   );
 }
