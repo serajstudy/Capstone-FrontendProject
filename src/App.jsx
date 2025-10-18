cf86be33034bbfc7e81fb7ee9ece129ef4d2fff8
seraj <serajstudy1@gmail.com>
2025-10-17 22:21:34 -0400

    working on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { Route, Routes } from "react-router-dom";
+import { BrowserRouter  } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './Components/Navbar';
 import CountryCard from './Components/CountryCard';
 import Footer from './Components/Footer';
+import Home from './Pages/Home';
+import Country from './Pages/Country';
+import Profile from './Pages/Profile';
+import Service from './Pages/Service';
+
 
 
 import './App.css'
@@ -12,16 +18,24 @@ function App() {
  
   return (
     <>
-    <Navbar/>
-    <CountryCard/>
-    <Footer/>
+      <Navbar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/countries" element={<Country />} />
+            <Route path="/services" element={<Service />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </main>
+        <Footer />
 
-
-
+  
+    
 
       
     </>
   )
 }
 
-export default App
+export default App;
+
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,36 +3,16 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <>
-
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <div className="container">
-        <Link className="navbar-brand fw-bold" to="/">BaseEducation</Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navMenu"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navMenu">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/countries">Countries</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/services">Services</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/profile">Profile</Link></li>
-          </ul>
-        </div>
+    <nav className="nav">
+      <div className="container nav-inner">
+        <div className="brand"><Link to="/">BaseEducation</Link></div>
+        <ul className="nav-links">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/countries">Countries</Link></li>
+          <li><Link to="/services">Services</Link></li>
+          <li><Link to="/profile">Profile</Link></li>
+        </ul>
       </div>
     </nav>
-    
-    
-    </>
-    
-    
-    
-   
   );
 }
